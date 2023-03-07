2403b19d82a9731098aa058ac422c4d289ba6e10
Jonathan <jonathan.valenciato@udlap.mx>
2023-03-07 16:44:37 -0600

    Initial Commit

---
+++ ProyectoPrueba2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProyectoPrueba2
+//
+//  Created by UDLAP14 on 07/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
